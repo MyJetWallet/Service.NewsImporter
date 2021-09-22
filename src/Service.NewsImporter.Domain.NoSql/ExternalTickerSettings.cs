fa44994351d06b4cd604ad670cd1cbcb063476e2
Alexey <alexey.p@smplt.net>
2021-09-22 19:20:07 +0300

    update settings dto

--- src/Service.NewsImporter.Domain.NoSql/ExternalTickerSettings.cs
+++ src/Service.NewsImporter.Domain.NoSql/ExternalTickerSettings.cs
@@ -6,7 +6,8 @@ namespace Service.NewsImporter.Domain.NoSql
     [DataContract]
     public class ExternalTickerSettings
     {
-        [DataMember(Order = 1)] public string NewsTicker { get; set; }
-        [DataMember(Order = 2)] public List<string> AssociateSymbols { get; set; }
+        [DataMember(Order = 1)] public string IntegrationSource { get; set; }
+        [DataMember(Order = 2)] public string NewsTicker { get; set; }
+        [DataMember(Order = 3)] public List<string> AssociateSymbols { get; set; }
     }
 }
